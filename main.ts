2d2940c0f75e5ecdc23f82219d5679f027d57fe0
Alex Nelson <alex.nelson@mercury-wave.com>
2023-01-31 22:14:03 -0600

    basic layout and textareas

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-console.log("yay!");
+class Workspace {
+    
+    private _paneMain : HTMLElement;
+    private _txtInput : HTMLTextAreaElement;
+    private _txtOutput : HTMLTextAreaElement;
+    private _txtEditor : HTMLTextAreaElement;
+
+    public constructor(pane: HTMLElement)
+    {
+        this._paneMain = pane;
+        this._txtInput = pane.appendChild(this.makeTextArea("txtIn",true));
+        this._txtOutput = pane.appendChild(this.makeTextArea("txtOut",false));
+        this._txtEditor = pane.appendChild(this.makeTextArea("txtEd",true));
+    }
+
+    private makeTextArea(className:string, hookEvents: boolean) {
+        let area = document.createElement("textarea");
+        area.setAttribute("spellcheck", "false");
+        area.className = className;
+        area.addEventListener("input", this.process);
+        return area;
+    }
+
+    public process(){
+        console.log("woot!");
+    }
+}
+
+let _instance : Workspace;
+document.addEventListener("DOMContentLoaded", () => {
+    _instance = new Workspace(document.getElementById("paneMain"));
+});
--- out/main.js
+++ out/main.js
@@ -1,2 +1,24 @@
-console.log("yay!");
+var Workspace = /** @class */ (function () {
+    function Workspace(pane) {
+        this._paneMain = pane;
+        this._txtInput = pane.appendChild(this.makeTextArea("txtIn", true));
+        this._txtOutput = pane.appendChild(this.makeTextArea("txtOut", false));
+        this._txtEditor = pane.appendChild(this.makeTextArea("txtEd", true));
+    }
+    Workspace.prototype.makeTextArea = function (className, hookEvents) {
+        var area = document.createElement("textarea");
+        area.setAttribute("spellcheck", "false");
+        area.className = className;
+        area.addEventListener("input", this.process);
+        return area;
+    };
+    Workspace.prototype.process = function () {
+        console.log("woot!");
+    };
+    return Workspace;
+}());
+var _instance;
+document.addEventListener("DOMContentLoaded", function () {
+    _instance = new Workspace(document.getElementById("paneMain"));
+});
 //# sourceMappingURL=main.js.map
