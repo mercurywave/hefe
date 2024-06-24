487b56a3ec2fe05d576a5c98e77ec1ce61e6b766
Alex Nelson <alex.nelson@mercury-wave.com>
2024-06-23 22:31:04 -0500

    basic sidebar UI

---
+++ out/sidebar.js
@@ -0,0 +1,96 @@
+export class Sidebar extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: "open" });
+        shadow.append(Sidebar._tmplt.content.cloneNode(true));
+        this.container = shadow.querySelector("#sidebar");
+        this.content = shadow.querySelector("#content");
+        this.btClose = shadow.querySelector("#closebtn");
+    }
+    connectedCallback() {
+        this.btClose.addEventListener("click", () => {
+            this.hide();
+        });
+        document.addEventListener("keydown", e => {
+            if (e.key === "Escape") {
+                this.hide();
+            }
+        });
+    }
+    show(content) {
+        this.content.innerHTML = content;
+        this.container.style.width = "350px";
+    }
+    hide() { this.container.style.width = "0"; }
+}
+Sidebar._tmplt = mkTmplt(`
+        <div id="sidebar">
+            <div id="label">Help</div>
+            <a href="javascript:void(0)" id="closebtn">×</a>
+            <div id="content">
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Clients</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+        <style>
+            #sidebar {
+                height: 100%;
+                width: 0;
+                position: fixed;
+                z-index: 1;
+                top: 0;
+                right: 0;
+                background-color: #111;
+                overflow-x: hidden;
+                transition: 0.5s;
+                padding-top: 60px;
+            }
+            #sidebar a {
+                padding: 8px 8px 8px 32px;
+                text-decoration: none;
+                font-size: 25px;
+                color: #818181;
+                display: block;
+                transition: 0.3s;
+            }
+            #sidebar a:hover {
+                color: #f1f1f1;
+            }
+            #label {
+                position: absolute;
+                top: 0;
+                left: 5px;
+                font-size: 36px;
+                padding: 8px;
+            }
+            #content {
+                padding: 15px;
+            }
+            #sidebar #closebtn {
+                position: absolute;
+                top: 0;
+                right: 5px;
+                font-size: 36px;
+            }
+            .openbtn {
+                font-size: 20px;
+                cursor: pointer;
+                background-color: #111;
+                color: white;
+                padding: 10px 15px;
+                border: none;
+            }
+            .openbtn:hover {
+                background-color: #444;
+            }
+        </style>
+    `);
+function mkTmplt(innerHtml) {
+    var tmplt = document.createElement("template");
+    tmplt.innerHTML = innerHtml;
+    return tmplt;
+}
+customElements.define("side-bar", Sidebar);
+//# sourceMappingURL=sidebar.js.map
